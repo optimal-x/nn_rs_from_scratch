e839c3a0c48224d1181b9942afcc560e0957f4f3
dannyhvc <danherrerav@gmail.com>
2025-03-03 17:22:53 -0500

    refac: changed dim to axis
    Signed-off-by: dannyhvc <danherrerav@gmail.com>

--- linalg/src/ndarr/mod.rs
+++ linalg/src/ndarr/mod.rs
@@ -5,7 +5,9 @@ pub mod nested_arr;
 use crate::shape::Shape;
 
 pub trait ArrD<T, const DIMS: usize> {
-    const DIMS: usize = DIMS;
     fn get(&self, indicies: &[usize]) -> Option<&T>;
     fn shape(&self) -> Shape<DIMS>;
+    fn axis(&self) -> usize {
+        DIMS
+    }
 }
